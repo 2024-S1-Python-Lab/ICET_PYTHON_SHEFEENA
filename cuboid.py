d2a9adcf24ca0f1d6fa345d369b246e8c1dc8e47
ShefeenaHaris <shefeenaharis02@gmail.com>
2025-01-03 14:55:39 +0530

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,7 @@
+def area(length,breadth,height):
+  area=2*(length*breadth+breadth*height+length*height)
+  return area
+
+def perimeter(length,breadth,height):
+    perimeter=4*(length+breadth+height)
+    return perimeter
